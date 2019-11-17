d9803c724e99dce620ba5825dba2b8f6ca6053ab
surajs02 <suraj02@hotmail.co.uk>
2019-11-17 22:20:41 +0000

    Add initial cmd & check dirs exist

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+tmuxtCmdPath='./tmuxt.sh'
+tmuxtDirPath='~/.tmuxt'
+templatesDirPath="$tmuxtDirPath/templates"
+
+function ensureDirExists {
+    if test ! -d "$1"; then
+        mkdir "$1"
+    fi
+}
+
+function isDirEmpty {
+    if [ -z "$(ls -A $1)" ]; then
+        return 0
+    fi
+    return 1
+}
+
+if test -f "$tmuxtCmdPath"; then
+    ensureDirExists "$tmuxtDirPath"
+    ensureDirExists "$templatesDirPath"
+else
+    echo "Cannot tmuxt command at $tmuxtCmdPath"
+    exit
+fi

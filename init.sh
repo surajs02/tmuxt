6aa7b588f932c6778a1b7bf11d561d2e6128b38a
surajs02 <suraj02@hotmail.co.uk>
2019-12-06 21:27:09 +0000

    Make autocomplete work (list templates dir contents)

--- init.sh
+++ init.sh
@@ -13,6 +13,19 @@ function isDirEmpty {
     return 1
 }
 
+function setupCompletion() {
+    # Autocomplete for tmuxt lists contents of templates dir
+    # TODO johto Make autocomplete list contents of current dir for relevant options (eg add)
+    _listTmuxtTemplates()
+    {
+        local cur=${COMP_WORDS[COMP_CWORD]}
+        # (Double quotes to preserve spaces for wordlist)
+        COMPREPLY=( $(compgen -W "$(ls $TMUXT_TEMPLATES_PATH)" -- $cur) )
+    }
+    # Tab autocomplete works when mapping fn to script name (not path)
+    complete -F _listTmuxtTemplates $(basename $TMUXT_CLI_PATH)
+}
+
 currentDir="$(dirname $BASH_SOURCE)" # current dir is based on where init.sh was launched from
 loadSettingsFile="$currentDir/loadSettings.sh"
 if ! test -f $loadSettingsFile; then
@@ -24,9 +37,7 @@ fi
 if test -f $TMUXT_CLI_PATH; then
     if ! test -f $TMUXT_TEMPLATES_PATH; then
         ensureDirExists $TMUXT_TEMPLATES_PATH
-        # Setup tab autocompletion
-        # (Double quotes to preserve spaces for wordlist)
-        complete -W "$TMUXT_TEMPLATES_PATH" $TMUXT_CLI_PATH
+        setupCompletion
     fi
 else
     echo "No tmuxt command found at [$TMUXT_CLI_PATH]"
